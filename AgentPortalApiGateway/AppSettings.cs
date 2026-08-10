cb1c891b4224d2b964463e49a4839214773940d4
dphan35 <daivan.phan2@dxc.com>
2026-08-10 17:03:19 +0700

    [000] Re-Structure Project

--- AgentPortalApiGateway/AppSettings.cs
+++ AgentPortalApiGateway/AppSettings.cs
@@ -3,4 +3,5 @@ namespace AgentPortalApiGateway;
 public class AppSettings
 {
     public string[] AllowedChatOrigins { get; set; }
+    public string Secret { get; set; }
 }
--- AgentPortalApiGateway/Program.cs
+++ AgentPortalApiGateway/Program.cs
@@ -1,16 +1,17 @@
-﻿using System.Text;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Eureka;
-using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
 
 namespace AgentPortalApiGateway;
 
@@ -23,44 +24,12 @@ public class Program
 
     public static IWebHost BuildWebHost(string[] args)
     {
-        var key = Encoding.ASCII.GetBytes("THIS_IS_A_RANDOM_SECRET_2e7a1e80-16ee-4e52-b5c6-5e8892453459");
 
         return WebHost.CreateDefaultBuilder(args)
             .ConfigureLogging(logging =>
             {
                 logging.ClearProviders();
                 logging.AddLog4Net("config/log4net.xml");
-            })
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config
-                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                    .AddYamlFile("config/appsettings.yaml", optional: false, reloadOnChange: true)
-                    .AddYamlFile($"config/appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.yaml", optional: true, reloadOnChange: true)
-                    .AddJsonFile($"config/ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                    .AddEnvironmentVariables();
-            })
-            .ConfigureServices(s =>
-            {
-                s.AddCors();
-                s.AddAuthentication(x =>
-                    {
-                        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    })
-                    .AddJwtBearer("ApiSecurity", x =>
-                    {
-                        x.RequireHttpsMetadata = false;
-                        x.SaveToken = true;
-                        x.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            ValidateIssuerSigningKey = true,
-                            IssuerSigningKey = new SymmetricSecurityKey(key),
-                            ValidateIssuer = false,
-                            ValidateAudience = false
-                        };
-                    });
-                s.AddOcelot().AddEureka().AddCacheManager(x => x.WithDictionaryHandle());
             })
             .Configure(a =>
             {
@@ -77,6 +46,42 @@ public class Program
                     .AllowCredentials()
                 );
                 a.UseOcelot().Wait();
+            })
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddYamlFile("config/appsettings.yaml", optional: false, reloadOnChange: true)
+                    .AddYamlFile($"config/appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.yaml", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"config/ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices((context, s) =>
+            {
+                var secret = context.Configuration["AppSettings:Secret"];
+                var key = Encoding.ASCII.GetBytes(secret);
+
+                s.AddCors();
+                s.AddAuthentication(x =>
+                    {
+                        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    })
+                    .AddJwtBearer("ApiSecurity", x =>
+                    {
+                        var appSettings = new AppSettings();
+                        var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+                        x.RequireHttpsMetadata = false;
+                        x.SaveToken = true;
+                        x.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(key),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                        };
+                    });
+                s.AddOcelot().AddEureka().AddCacheManager(x => x.WithDictionaryHandle());
             })
             .Build();
     }
